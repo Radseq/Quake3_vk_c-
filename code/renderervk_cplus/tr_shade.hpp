f9eaf55e8616b6f42942c76a1661f626227ac343
Radseq <radek.g.10@gmail.com>
2024-04-17 08:01:14 +0200

    changes in tr_shade files

--- code/renderervk_cplus/tr_shade.hpp
+++ code/renderervk_cplus/tr_shade.hpp
@@ -11,6 +11,12 @@ extern "C"
     void R_ComputeTexCoords_plus(const int b, const textureBundle_t *bundle);
     void VK_SetFogParams_plus(vkUniform_t *uniform, int *fogStage);
     void R_ComputeColors_plus(const int b, color4ub_t *dest, const shaderStage_t *pStage);
+    uint32_t VK_PushUniform_plus(const vkUniform_t *uniform);
+#ifdef USE_PMLIGHT
+    void VK_LightingPass_plus(void);
+#endif // USE_PMLIGHT
+    void RB_StageIteratorGeneric_plus(void);
+    void RB_EndSurface_plus(void);
 
 #ifdef __cplusplus
 }
