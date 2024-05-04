cf47a60500f3d49e98ae4a0a5e8396664f1218d0
Radseq <radek.g.10@gmail.com>
2024-05-04 17:24:49 +0200

    more #ifdef __cplusplus deletions

--- code/renderervk_cplus/tr_common.hpp
+++ code/renderervk_cplus/tr_common.hpp
@@ -22,11 +22,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #ifndef TR_COMMON_HPP
 #define TR_COMMON_HPP
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
 #define USE_VULKAN
 
 #include "../qcommon/q_shared.h"
@@ -34,29 +29,29 @@ extern "C"
 
 #define MAX_TEXTURE_UNITS 8
 
-	typedef enum
-	{
-		IMGFLAG_NONE = 0x0000,
-		IMGFLAG_MIPMAP = 0x0001,
-		IMGFLAG_PICMIP = 0x0002,
-		IMGFLAG_CLAMPTOEDGE = 0x0004,
-		IMGFLAG_CLAMPTOBORDER = 0x0008,
-		IMGFLAG_NO_COMPRESSION = 0x0010,
-		IMGFLAG_NOLIGHTSCALE = 0x0020,
-		IMGFLAG_LIGHTMAP = 0x0040,
-		IMGFLAG_NOSCALE = 0x0080,
-		IMGFLAG_RGB = 0x0100,
-		IMGFLAG_COLORSHIFT = 0x0200,
-	} imgFlags_t;
+typedef enum
+{
+	IMGFLAG_NONE = 0x0000,
+	IMGFLAG_MIPMAP = 0x0001,
+	IMGFLAG_PICMIP = 0x0002,
+	IMGFLAG_CLAMPTOEDGE = 0x0004,
+	IMGFLAG_CLAMPTOBORDER = 0x0008,
+	IMGFLAG_NO_COMPRESSION = 0x0010,
+	IMGFLAG_NOLIGHTSCALE = 0x0020,
+	IMGFLAG_LIGHTMAP = 0x0040,
+	IMGFLAG_NOSCALE = 0x0080,
+	IMGFLAG_RGB = 0x0100,
+	IMGFLAG_COLORSHIFT = 0x0200,
+} imgFlags_t;
 
-	typedef enum
-	{
-		CT_FRONT_SIDED = 0,
-		CT_BACK_SIDED,
-		CT_TWO_SIDED
-	} cullType_t;
+typedef enum
+{
+	CT_FRONT_SIDED = 0,
+	CT_BACK_SIDED,
+	CT_TWO_SIDED
+} cullType_t;
 
-	typedef struct image_s image_t;
+typedef struct image_s image_t;
 
 // any change in the LIGHTMAP_* defines here MUST be reflected in
 // R_FindShader() in tr_bsp.c
@@ -65,56 +60,52 @@ extern "C"
 #define LIGHTMAP_WHITEIMAGE -2
 #define LIGHTMAP_NONE -1
 
-	extern glconfig_t glConfig; // outside of TR since it shouldn't be cleared during ref re-init
+extern glconfig_t glConfig; // outside of TR since it shouldn't be cleared during ref re-init
 
-	// These variables should live inside glConfig but can't because of
-	// compatibility issues to the original ID vms.  If you release a stand-alone
-	// game and your mod uses tr_types.h from this build you can safely move them
-	// to the glconfig_t struct.
-	extern bool textureFilterAnisotropic;
-	extern int maxAnisotropy;
+// These variables should live inside glConfig but can't because of
+// compatibility issues to the original ID vms.  If you release a stand-alone
+// game and your mod uses tr_types.h from this build you can safely move them
+// to the glconfig_t struct.
+extern bool textureFilterAnisotropic;
+extern int maxAnisotropy;
 
-	//
-	// cvars
-	//
-	extern cvar_t *r_stencilbits; // number of desired stencil bits
-	extern cvar_t *r_texturebits; // number of desired texture bits
-								  // 0 = use framebuffer depth
-								  // 16 = use 16-bit textures
-								  // 32 = use 32-bit textures
-								  // all else = error
+//
+// cvars
+//
+extern cvar_t *r_stencilbits; // number of desired stencil bits
+extern cvar_t *r_texturebits; // number of desired texture bits
+							  // 0 = use framebuffer depth
+							  // 16 = use 16-bit textures
+							  // 32 = use 32-bit textures
+							  // all else = error
 
-	extern cvar_t *r_drawBuffer;
+extern cvar_t *r_drawBuffer;
 
-	extern cvar_t *r_allowExtensions;		  // global enable/disable of OpenGL extensions
-	extern cvar_t *r_ext_compressed_textures; // these control use of specific extensions
-	extern cvar_t *r_ext_multitexture;
-	extern cvar_t *r_ext_compiled_vertex_array;
-	extern cvar_t *r_ext_texture_env_add;
+extern cvar_t *r_allowExtensions;		  // global enable/disable of OpenGL extensions
+extern cvar_t *r_ext_compressed_textures; // these control use of specific extensions
+extern cvar_t *r_ext_multitexture;
+extern cvar_t *r_ext_compiled_vertex_array;
+extern cvar_t *r_ext_texture_env_add;
 
-	extern cvar_t *r_ext_texture_filter_anisotropic;
-	extern cvar_t *r_ext_max_anisotropy;
+extern cvar_t *r_ext_texture_filter_anisotropic;
+extern cvar_t *r_ext_max_anisotropy;
 
-	// font stuff
+// font stuff
 
-	/*
-	=============================================================
+/*
+=============================================================
 
-	IMAGE LOADERS
+IMAGE LOADERS
 
-	=============================================================
-	*/
+=============================================================
+*/
 
-	/*
-	====================================================================
+/*
+====================================================================
 
-	IMPLEMENTATION SPECIFIC FUNCTIONS
+IMPLEMENTATION SPECIFIC FUNCTIONS
 
-	====================================================================
-	*/
-
-#ifdef __cplusplus
-}
-#endif
+====================================================================
+*/
 
 #endif // TR_COMMON_HPP
--- code/renderervk_cplus/tr_light.hpp
+++ code/renderervk_cplus/tr_light.hpp
@@ -1,25 +1,14 @@
 #ifndef TR_LIGHT_HPP
 #define TR_LIGHT_HPP
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
-#include "../qcommon/q_shared.h"
-
 #include "tr_local.hpp"
 
-    void R_TransformDlights(int count, dlight_t *dl, orientationr_t *ort);
+void R_TransformDlights(int count, dlight_t *dl, orientationr_t *ort);
 #ifdef USE_LEGACY_DLIGHTS
-    void R_DlightBmodel(bmodel_t *bmodel);
+void R_DlightBmodel(bmodel_t *bmodel);
 #endif // USE_LEGACY_DLIGHTS
 
-    int R_LightForPoint(vec3_t point, vec3_t ambientLight, vec3_t directedLight, vec3_t lightDir);
-    void R_SetupEntityLighting(const trRefdef_t *refdef, trRefEntity_t *ent);
-
-#ifdef __cplusplus
-}
-#endif
+int R_LightForPoint(vec3_t point, vec3_t ambientLight, vec3_t directedLight, vec3_t lightDir);
+void R_SetupEntityLighting(const trRefdef_t *refdef, trRefEntity_t *ent);
 
 #endif // TR_LIGHT_HPP
